1e2e3afe5b7961da69e02a11088f712c653ac6e8
SatLibRe <miles.higbie@gmail.com>
2020-11-29 15:08:32 -0500

    fixed batching overwrite

--- python_test_server.py
+++ python_test_server.py
@@ -15,9 +15,16 @@ identities.customerid = '123456'
 identities.email = 'user@example.com'
 batch.user_identities = identities
 
+#deviceinfo
+device_info = mparticle.DeviceInformation()
+# set any IDs that you have for this user
+device_info.ios_advertising_id = '07d2ebaa-e956-407e-a1e6-f05f871bf4e2'
+device_info.android_advertising_id = 'a26f9736-c262-47ea-988b-0b0504cee874'
+batch.device_info = device_info
+
 # custom event
 app_event = mparticle.AppEvent('Example', 'navigation')
-batch.events = [app_event]
+
 
 #commerce event
 product = mparticle.Product()
@@ -33,7 +40,6 @@ product_action.total_amount = 21.49
 commerce_event = mparticle.CommerceEvent(product_action)
 # commerce_event.timestamp_unixtime_ms = example_timestamp
 
-batch.events = [commerce_event]
 
 #sessions
 session_start = mparticle.SessionStartEvent()
@@ -45,7 +51,9 @@ session_end.session_id = session_start.session_id # its mandatory that these mat
 # session_end.session_duration_ms = example_duration
 # session_end.timestamp_unixtime_ms = example_timestamp + example_duration
 
-batch.events = [session_start, session_end]
+batch.events = [app_event, commerce_event, session_start, session_end]
+
+print("im the batch events", batch.events)
 
 
 try: 
